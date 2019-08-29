40e527b5c48e1e9d6f24d715a9ae974cf970493f
Logan Karnes <logan.karnes98@gmail.com>
2019-08-28 20:54:34 -0700

    updated profile page

--- src/components/ProfilePage.js
+++ src/components/ProfilePage.js
@@ -16,32 +16,22 @@ const ProfilePage = () => {
         axios
         .get('https://storytelling-back-end.herokuapp.com/api/stories/user/stories', config())
         .then(res => {
-            console.log(res)
+            console.log(res, 'res')
             setStories(res.data)
         })
     }, [])
     if(!user) return <div>Loading...</div>
-    else if(user.type==='coordinator') 
     return (
         <div>
-            <h1>{user.firstName} {user.lastName} Coordinator Profile</h1>
+            <h1>{user.firstName} {user.lastName} Profile</h1>
             <h2>Username: {user.username}</h2>
             <h2>Email: {user.email}</h2>
             <h2>{user.country}</h2>
             <h2>{user.workTitle}</h2>
             <h1>Saved Stories</h1>
             {stories.map(story=>(
-                <div>{story.story}</div>
+                <div>{story.title}</div>
             ))}
-        </div>
-    )
-    else
-    return (
-        <div>
-            <h1>{user.firstName}</h1>
-            <h1>{user.lastName}</h1>
-            <h2>{user.country}</h2>
-
         </div>
     )
 }
