2aa3e23040636d24db8cd2503532e5f5373da8cf
Logan Karnes <logan.karnes98@gmail.com>
2019-08-29 23:34:47 -0700

    country tabs functionality complete, save complete user stories display on home page

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -1,10 +1,12 @@
 import React, {useState, useEffect} from 'react'
 import axios from 'axios'
+import {NavLink} from 'react-router-dom';
+
 
 const Feed = () => {
     const [stories, setStories] = useState()
+    const [country, setCountry] = useState('all')
     console.log(stories)
-    
     useEffect(() => {
         axios
         .get("https://storytelling-back-end.herokuapp.com/api/stories")
@@ -12,16 +14,45 @@ const Feed = () => {
             setStories(res.data)
         })
     }, [])
-
     if(!stories) return <div>Loading...</div>
     return (
         <div>
-            {stories.map(each => (
+            <div className='country-tabs'>
+                <button onClick={()=>{setCountry('all')}} className='nav-link'>All</button>
+                <button onClick={()=>{setCountry('Bolivia')}} className='nav-link'>bolivia</button>
+                <button onClick={()=>{setCountry('brazil')}} className='nav-link'>brazil</button>
+                <button onClick={()=>{setCountry('cambodia')}} className='nav-link'>cambodia</button>
+                <button onClick={()=>{setCountry('colombia')}} className='nav-link'>colombia</button>
+                <button onClick={()=>{setCountry('ecuador')}} className='nav-link'>ecuador</button>
+                <button onClick={()=>{setCountry('el salvador')}} className='nav-link'>el salvador</button>
+                <button onClick={()=>{setCountry('ghana')}} className='nav-link'>ghana</button>
+                <button onClick={()=>{setCountry('guatemala')}} className='nav-link'>guatemala</button>
+                <button onClick={()=>{setCountry('haiti')}} className='nav-link'>haiti</button>
+                <button onClick={()=>{setCountry('honduras')}} className='nav-link'>honduras</button>
+                <button onClick={()=>{setCountry('kiribati')}} className='nav-link'>kiribati</button>
+                <button onClick={()=>{setCountry('madagascar')}} className='nav-link'>madagascar</button>
+                <button onClick={()=>{setCountry('mongolia')}} className='nav-link'>mongolia</button>
+                <button onClick={()=>{setCountry('nicaragua')}} className='nav-link'>nicaragua</button>
+                <button onClick={()=>{setCountry('paraguay')}} className='nav-link'>paraguay</button>
+                <button onClick={()=>{setCountry('peru')}} className='nav-link'>peru</button>
+                <button onClick={()=>{setCountry('philippines')}} className='nav-link'>philippines</button>
+                <button onClick={()=>{setCountry('sierra leone')}} className='nav-link'>sierra leone</button>
+                <button onClick={()=>{setCountry('zimbabwe')}} className='nav-link'>zimbabwe</button>
+
+            </div>
+            {stories.map((each) => {
+                console.log(each.country)
+                console.log(country)
+                if(each.country === country || country === 'all'){
+                    return(
                 <div key={each.id}>
                     <h2>{each.story}</h2>
                     <h5>{each.posted_by}</h5>
+                    <NavLink to={`/feed/${each.id}`}>Full Page View</NavLink>
                 </div>
-            ))}
+                    )
+                }
+            })}
         </div>
     )
 }
--- src/components/ProfilePage.js
+++ src/components/ProfilePage.js
@@ -5,19 +5,24 @@ import axios from 'axios';
 const ProfilePage = () => {
     const [user, setUser] = useState()
     const [stories, setStories]= useState([])
+    const [saved, setSaved]= useState([])
 
     useEffect(() => {
         axios
         .get('https://storytelling-back-end.herokuapp.com/api/users/logged', config())
         .then(res => {
             setUser(res.data)
-            console.log(res)
         })
         axios
         .get('https://storytelling-back-end.herokuapp.com/api/stories/user/stories', config())
         .then(res => {
-            console.log(res, 'res')
             setStories(res.data)
+        })
+        axios
+        .get('https://storytelling-back-end.herokuapp.com/api/stories/saved', config())
+        .then(res=>{
+            console.log(res)
+            setSaved(res.data)
         })
     }, [])
     if(!user) return <div>Loading...</div>
@@ -27,16 +32,23 @@ const ProfilePage = () => {
                 <h1>{user.firstName} {user.lastName} Profile</h1>
             </div>
             <div  className='donor-info'>
-                <p>Username: {user.username}</p>
-                <p>Email: {user.email}</p>
-                <p>Country: {user.country}</p>
-                <p> Worktitle: {user.workTitle}</p>
+                <div className='info-box'>
+                    <p>Username: {user.username}</p>
+                    <p>Email: {user.email}</p>
+                    <p>Country: {user.country}</p>
+                    <p> Worktitle: {user.workTitle}</p>
+                </div>
+
                 <h1>Saved Stories</h1>
-            {stories.map(story=>(
-                <div className='saved-item'>{story.title}</div>
-            ))}
-        </div>
+                {saved.map(story=>(
+                    <p>{story.title}</p>
+                ))}
+                <h1>user stories</h1>
+                {stories.map(story=>(
+                    <div>{story.title}</div>
+                ))}
             </div>
+        </div>
             
     )
 }
